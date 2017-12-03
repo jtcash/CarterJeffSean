cbaca2eea30871b56ac710f234b214b8eb75510d
Jeffrey Cash <jeffreytcash@gmail.com>
2017-12-03 15:45:19 -0800

    index click logging

--- 2/custom_tracking.js
+++ 2/custom_tracking.js
@@ -1,3 +1,8 @@
+function echo(arg){
+	console.log(arg);
+}
+
+
 function set_uid(uid){ localStorage["tracking_uid"] = uid; }
 function reset_uid(){ localStorage.removeItem("tracking_uid"); }
 function get_uid_string(){ return localStorage["tracking_uid"]; }
@@ -25,7 +30,7 @@ function send_event(event){
 		//tracker.send('event', event category, event data);
 
 		tracker.send('event', get_uid(), event);
-		console.log("sent event:{" + get_uid() + ", " + event + "}");
+		echo("sent event:{" + get_uid() + ", " + event + "}");
 	} else {
 		console.log("event tracking is not available")
 	}
@@ -54,20 +59,46 @@ function is_integer(value) {
 }
 
 
+
+
 // Record to the click list that there was a click at location
 function record_click(location){
+	echo("recording click:{" + location + "}");
+
 	var click_list = get_click_list();
 	if( !is_integer(click_list[location]) ){
 		click_list[location] = 1;
 	} else {
 		++click_list[location];
 	}
+	if(!is_integer(click_list._total)){
+		click_list._total = 1;
+	} else {
+		++click_list._total;
+	}
+
+	if(click_list._list == null || !(click_list._list.constructor === Array) ){
+		click_list._list = [];
+	}
+	click_list._list.push({t: Date.now(), l: location});
+
 	set_click_list(click_list);
-	return click_list[location];
+	return [ click_list[location], click_list._total ];
 }
 
 
 
+
+
+function send_click_list(){
+	echo("sending click list");
+
+	var click_list = get_click_list();
+	if(click_list._total > 0){
+		send_event(JSON.stringify(click_list));
+	}
+	reset_click_list();
+}
 
 
 
