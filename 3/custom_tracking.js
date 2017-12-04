b27703fd8c9f772727a3a72eb7b147c98045001f
Jeffrey Cash <jeffreytcash@gmail.com>
2017-12-04 00:10:15 -0800

    SO MANY FILES

---
+++ 3/custom_tracking.js
@@ -0,0 +1,173 @@
+function echo(arg){
+	console.log(arg);
+}
+
+
+function set_uid(uid){ localStorage["tracking_uid"] = uid; }
+function reset_uid(){ localStorage.removeItem("tracking_uid"); }
+function get_uid_string(){ return localStorage["tracking_uid"]; }
+function has_uid(){	return !(get_uid_string() == null); }
+function generate_uid(){ 
+	return Math.round(Math.random()) + '_' + Math.random().toString(36).substring(6) + '_ABtest2';
+}
+function get_uid(){
+	if(!has_uid()) set_uid(generate_uid());
+	return get_uid_string();
+}
+
+//Returns 0 or 1 for page version for the current user based on uid
+function get_page_version(){
+	var version = Number(get_uid().charAt(0));
+	if(version != 0 && version != 1){
+		console.log("UID IS MESSED UP; defaulting to version 0");
+		return 0;
+	}
+	return version;
+}
+
+
+function send_event(event){
+	if("ga" in window){
+		var tracker = ga.getAll()[0];
+		//tracker.send('event', event category, event data);
+
+		tracker.send('event', get_uid(), event);
+		echo("sent event:{" + get_uid() + ", " + event + "}");
+	} else {
+		console.log("event tracking is not available")
+	}
+}
+
+
+
+function reset_click_list(){ localStorage.removeItem("click_list"); }
+function set_click_list_string(click_list_string){ localStorage["click_list"] = click_list_string; }
+function set_click_list(click_list){ set_click_list_string(JSON.stringify(click_list)); }
+function get_click_list_string(){ return localStorage["click_list"]; }
+function get_click_list(){
+	var cl_string = get_click_list_string();
+	if(cl_string == null){
+		set_click_list({});
+		return get_click_list();
+	}
+	return JSON.parse(cl_string);
+}
+
+// Check if a variable is an integer
+function is_integer(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
+
+
+
+// Record to the click list that there was a click at location
+function record_click(location){
+	echo("recording click:{" + location + "}");
+
+	var click_list = get_click_list();
+	if(!is_integer(click_list._total)){
+		click_list._total = 1;
+	} else {
+		++click_list._total;
+	}
+
+	if( !is_integer(click_list[location]) ){
+		click_list[location] = 1;
+	} else {
+		++click_list[location];
+	}
+	
+
+	if(click_list._list == null || !(click_list._list.constructor === Array) ){
+		click_list._list = [];
+	}
+	click_list._list.push({t: Date.now(), l: location});
+
+	set_click_list(click_list);
+	return [ click_list[location], click_list._total ];
+}
+
+
+
+
+
+function send_click_list(){
+	echo("sending click list");
+
+	var click_list = get_click_list();
+	if(click_list._total > 0){
+		send_event(JSON.stringify(click_list));
+	}
+	reset_click_list();
+}
+
+
+
+
+
+function initialize_events(){
+	setTimeout(function() { send_event('Loaded_Index:' + Date.now()); send_click_list(); }, 100);
+}
+
+
+function reset(){
+	reset_uid();
+	reset_click_list();
+	reset_ls_patients();
+}
+
+
+
+
+
+// function alert_analytics(){
+
+// }
+// function ensure_analytics(){
+// 	if(!("ga" in window)){
+// 		alert("")
+// 	}
+// }
+
+function displayed_version(){
+	switch(window.location.pathname.replace(/.*\/CarterJeffSean\//, '').charAt(0)){
+		default:
+		case "c":
+			return 0; 
+		case "2":
+			return 1;
+	}
+}
+
+function tracking_redirect(){
+	return;
+	echo("called tracking_redirect");
+
+	echo("test:" + ("ga" in window));
+
+
+	var desired = get_page_version();
+	var displayed = displayed_version();
+
+	if(desired != displayed){
+		if(desired == 0){
+			window.location = window.location.pathname.replace(/\/2/,'');
+		} else {
+			window.location = window.location.pathname.replace(/CarterJeffSean\//, 'CarterJeffSean/2/');
+		}
+
+	} 
+
+	// var current_path = window.location.pathname;
+	
+
+	// setTimeout(function() { send_event('Loaded_Index:' + Date.now()); send_click_list(); }, 100);
+
+}
+
+
+
+
---
+++ 3/temp.js
@@ -0,0 +1,411 @@
+var patients_default = [
+		 {patient_name : 'Grandma', patient_meds : '1x Aricept, 1x Exelon, 1x Razadyne', patient_phone: '847-987-4238', color_status: 'red'}
+		,{patient_name : 'G-pa', patient_meds : '1x Aricept, 1x Exelon', patient_phone: '847-987-4238', color_status: 'yellow'}
+	];
+
+var patients = patients_default.slice();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////
+
+
+
+
+
+
+// Getters and setters for user_email
+function get_user_email(){ return localStorage['logged_in_as']; }
+function set_user_email(new_email){	localStorage['logged_in_as'] = (new_email == null ? "" : new_email); }
+function clear_user_email(){ set_user_email(""); }
+// Return boolean value of whether or not the user is already logged in
+function is_logged_in(){
+	var t = get_user_email();
+	return !(t == null || t == "");
+}
+
+
+
+function make_user(email, password, patients){
+	if(typeof patients === 'undefined') patients = [];
+	return {email:email, password:password, patients:patients}
+}
+
+function reset_user_array(){
+	console.log("resetting user array");
+	localStorage['users'] = '{}';
+}
+
+//Getter to retrieve the users array from localstorage as an array
+function get_user_array(){
+	var user_array_string = localStorage['users'];
+	if(user_array_string == null){
+		reset_user_array();
+		return [];
+	}
+	var user_array = [];
+	try{
+		user_array = JSON.parse(user_array_string);
+	} catch(err){
+		reset_user_array();
+		return [];
+	}
+	return user_array;
+}
+//Setter to put a user array into localstorage
+function set_user_array(user_array){
+	if(user_array == null){
+		console.log("trying to set users to null. Why do that???");
+		return false;
+	}
+
+	try{
+		var users_string = JSON.stringify(user_array);
+	}catch(err){
+		console.log("failed to stringify user_array");
+		return false;
+	}
+
+	localStorage['users'] = users_string;
+	return true; 
+}
+
+// Boolean to check if a user exists by email
+function user_exists(email){
+	var user_array = get_user_array();
+	if(user_array.length > 0){
+		for(var i=0; i<user_array.length; ++i){
+			if(user_array[i].email == email) return true;
+		}
+	}
+	return false;
+}
+
+function get_user(email){
+	var users = get_user_array();
+	for(var i=0; i<users.length; ++i){
+		if(users[i].email == email)
+			return users[i];
+	}
+	return null;
+}
+
+function password_is_valid(user, password){
+	return user.password == password;
+}
+
+
+// Add a user to localstorage, patients array optional
+function add_user(email, password, patients){
+	if(user_exists(email)){
+		alert("User already exists");
+		return false;
+	}
+
+	var new_user = make_user(email, password, patients);
+
+	var user_array = get_user_array();
+
+	// if(user_array.isArray){
+		try{
+			user_array.push(new_user);
+		} catch(err) {
+			user_array = [ new_user ];
+		}
+	// } else {
+	// 	user_array = [ new_user ];
+	// }
+	set_user_array(user_array);
+
+	console.log("users:{" + localStorage['users'] + '}');
+
+
+	return true;
+}
+
+
+function log_in_as(email){
+	set_user_email(email);
+	window.location = "index.html";
+	return true;
+}
+
+function attempt_login(){
+	var email = document.getElementById('login_email').value;
+	var password = document.getElementById('login_password').value;
+
+	console.log('attempting login with email:{' + email + '}, password:{' + password + '} lol so secure');
+
+
+	// Handle default login case
+	if((email.length == 0 || email == null) && (password.length == 0 || password==null)){
+		return log_in_as("default");
+	}	
+
+	// Handle actual login cases
+	if(!user_exists(email)){
+		alert('email is invalid');
+		return false;
+	}
+
+	var user = get_user(email);
+
+	//ensure password is correct
+	if(!password_is_valid(user, password)){
+		alert('incorrect password');
+		return false;
+	}
+
+
+
+	return log_in_as(email);
+}
+
+function log_out(){
+	clear_user_email(); 
+	window.location = "ct_login.html";
+}
+
+
+
+
+
+// Initialize default user stuff if first time running
+function initialize_shit(){
+	if(!user_exists('default')){
+		add_user('default', '', patients_default);
+	}
+}
+
+// Remove a user from ls by email
+function remove_user(email){
+	var user_array = get_user_array();
+	for(var i=0; i<user_array.length; ++i){
+		if(user_array[i].email == email){
+			user_array.splice(i,1);
+			set_user_array(user_array);
+			if(is_logged_in() && get_user_email() == email){
+				log_out();
+			}
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
+function get_ls_patients(){
+	if(!is_logged_in()) return [];
+
+	var user = get_user(get_user_email());
+	if(user == null){
+		return [];
+	}
+	var patients = user.patients;
+	if(patients == null) return [];
+
+	for(var i=0; i<patients.length; ++i){
+		patients[i].patient_id = i;
+	}
+
+	return patients;
+}
+function set_ls_patients(patients){
+	if(!is_logged_in()) return false;
+	var email = get_user_email();
+	var user_array = get_user_array();
+	for(var i=0; i<user_array.length; ++i){
+		if(user_array[i].email == email){
+			user_array[i].patients = patients;
+			set_user_array(user_array);
+			return true;
+		}
+	}
+	return false;
+}
+
+function reset_ls_patients(){
+	set_ls_patients(get_user_email() == 'default' ? patients_default : []);
+
+}
+
+
+/////////////////
+
+
+function call_patient(patient_id){
+	
+	var patient = get_ls_patients()[patient_id];
+	// alert("calling " +  patient.patient_name + " (not yet implemented): " + patient.patient_phone);
+	window.open("tel:+1" + patient.patient_phone);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Change the status of a patient (change indicator color)
+function change_status(id, color){
+	// Ensure a valid color is chosen
+	switch(color){
+		case 'red':
+		case 'yellow':
+		case 'green':
+			break;
+		default:
+			console.log("invalid status color chosen; defaulting to yellow");
+			color = 'yellow';
+	}
+
+	var patients = get_ls_patients();
+	
+	patients[id]['color_status'] = color;
+	
+	document.getElementById("status_image_" + id).src = "img/dot_big_" + color + ".png";
+
+
+}
+// Change the status of a patient after some delay
+function change_status_delay(id, color, delay_ms){
+	setTimeout(function(){ change_status(id, color); }, delay_ms);
+}
+
+
+
+
+// Create the patient fields on index.html from an array of patients
+function create_patients(patient_array){
+	// console.log('creating patients');
+	var source = $("#patient_template").html();
+	var template = Handlebars.compile(source);
+
+	var parent_div = $("#templated_patients");
+
+	// Clear the div in case there's junk in it for some reason
+	parent_div.empty();
+	for(var i=0; i<patient_array.length; ++i) parent_div.append(template(patient_array[i]));
+
+	// for(var i=0; i<patient_array.length; ++i){
+	// 	 var cur_data = patient_array[i]; 
+	// 	 var cur_html = template(cur_data); 
+	// 	 parent_div.append(cur_html);			
+	// }
+	// console.log('done creating patients');
+}
+
+// Handler for when add medications is pressed
+function add_meds_handler(patient_id){
+	console.log('button pressed from patient_id=' + patient_id);
+	var patients = get_ls_patients();
+	$("#meds_input_"+patient_id).val(patients[patient_id]['patient_meds']);
+}
+
+
+
+
+function add_patient(name, meds, phone) {
+	// var name = document.getElementById('name').value;
+	// var meds = document.getElementById('meds').value;
+	var patients_array = get_ls_patients();
+	for(var i=0; i<patients_array.length; ++i){
+		if(name == patients_array[i].patient_name){
+			alert("duplicate patient");
+			return false;
+		}
+	}
+	var new_patient = {patient_name: name, patient_meds: meds, patient_phone: phone, color_status: 'red'};
+	patients_array.push(new_patient);
+
+
+	set_ls_patients(patients_array);
+	return true;
+
+	// localStorage.setItem("patients", JSON.stringify(patients_array)); 
+}
+
+
+function save_meds(patient_id){
+	
+	var new_meds = $("#meds_input_"+patient_id).val();
+	// console.log("saving meds for id=" + patient_id+"\nneed to also write to localstorage");
+
+	var patient_array = get_ls_patients();
+
+
+	patient_array[patient_id].patient_meds = new_meds;
+	document.getElementById('patient_meds_' + patient_id).innerText = new_meds;
+	// $("#patient_meds_"+patient_id).val(new_meds);
+
+	set_ls_patients(patient_array);
+
+	// patients[patient_id]['patient_meds'] = new_meds;
+
+	// // $("#patient_meds_" + patient_id).val(new_meds);
+	// document.getElementById("patient_meds_"+patient_id).innerText = new_meds;
+	// console.log("TODO: update localstorage");
+
+	// add_patient(patients[patient_id]["patient_name"], new_meds);
+
+
+}
+
+
+
+function add_patient_to_global(patient){
+	var name = patient['patient_name'];
+	var meds = patient['patient_meds'];
+	var phone = patient['patient_phone'];
+
+	var exists = false;
+	console.log("updating patients with: " + name +", " + meds);
+	for(var i=0; i<patients.length; ++i){
+		// console.log("name == patients[i]['patient_name']:" + name + "==" + patients[i]['patient_name']);
+		if(name == patients[i]['patient_name']){
+			console.log("WTF")
+			patients[i]['patient_meds'] = meds;
+			patients[i]['patient_phone'] = phone;
+			exists = true;
+		}
+	}
+	if(!exists){
+		//var new_patient = {'patient_name' : name, 'patient_meds' : meds, 'color_status': 'green'};
+		patients.push(patient);
+	}
+}
+function modify_patients_from_localstorage(){
+	// console.log("WTF DID THIS DO");
+	var patients_string = localStorage["patients"];
+	// console.log("WTF: " + patients_string);
+	var patients_array = [];
+	if( patients_string != null && patients_string != undefined ){
+		patients_array = JSON.parse(patients_string);
+		if(patients_array == null) patients_array = [];
+	}
+
+	// console.log("patients_array.length=" + patients_array.length)
+	for(var i=0; i<patients_array.length; ++i){
+		add_patient_to_global(patients_array[i]);
+	}
+}
+
+
+
